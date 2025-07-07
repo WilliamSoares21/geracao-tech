32ba749ec545f5157b7d6a965f513c9f3fa31973
William Soares <pedro.williamsoares2022@gmail.com>
2025-07-07 14:41:33 -0300

    Content Update

--- javascript/POO/introducao.js
+++ javascript/POO/introducao.js
@@ -1,4 +1,4 @@
-let pessoa = {
+/*let pessoa = {
   nome: "Paul",
   idade: "18",
   genero: "masculino",
@@ -9,3 +9,28 @@ console.log(pessoa.nome);
 console.log(pessoa.idade);
 console.log(pessoa.genero);
 console.log(pessoa.habilidades);
+*/
+
+// Exemplo de objetos e funções (anônimas e arrow)
+
+let pessoa = {
+  nome: "Paul",
+  idade: "18",
+  genero: "masculinolino",
+  habilidades: ["presciência", "ver o futuro", "domínio a Voz"],
+  saudacao: function () {
+    return `Olá meu noome é ${this.nome} e eu tenho ${this.idade} anos.`;
+  },
+  adicionarHabilidade: function (habilidade) {
+    this.habilidades.push(habilidade);
+  },
+  listarHabilidades: () => {
+    console.log("Minhas habilidades são: " + pessoa.habilidades.join(", "));
+  }
+};
+
+console.log(pessoa.saudacao());
+pessoa.adicionarHabilidade(("Dominação de Vermes"));
+pessoa.listarHabilidades();
+
+
